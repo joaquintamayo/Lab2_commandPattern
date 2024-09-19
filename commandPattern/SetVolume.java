c7207d7920e7173c294bf7ff0eba986883228975
joaquintamayo <152839460+joaquintamayo@users.noreply.github.com>
2024-09-19 11:34:48 +0000

    commit

---
+++ commandPattern/SetVolume.java
@@ -0,0 +1,14 @@
+public class SetVolume implements Command {
+    private MusicPlayer musicPlayer;
+    private int volume;
+
+    public SetVolume(MusicPlayer musicPlayer, int volume) {
+        this.musicPlayer = musicPlayer;
+        this.volume = volume;
+    }
+
+    @Override
+    public void execute() {
+        musicPlayer.setVolume(volume);
+    }
+}
